79a8368f68d1ea1e0be3a0c63bc24c25060fb97a
Karthik-B-06 <karthik06051996@gmail.com>
2022-10-04 23:13:33 +0530

    feat: :sparkles: add Layout to Next App

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,14 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head>
+      <Html className="h-full bg-gray-100 antialiased" lang="en">
+        <Head title="Friendly Goggles">
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
           />
         </Head>
-        <body>
+        <body className="flex h-full flex-col">
           <Main />
           <NextScript />
         </body>
