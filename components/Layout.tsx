4029fdb0c83c2a4d38a3e3358f81287ad7be49b4
Karthik-B-06 <karthik06051996@gmail.com>
2022-10-06 18:56:09 +0530

    fix: :bug: fix for zIndex issue on shared element transition

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
@@ -10,6 +10,9 @@ import { AspectBlurImage } from "./AspectBlurImage";
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   const router = useRouter();
   const isOpen = router.asPath !== "/";
+  const [currentMovieId, setCurrentMovieId] = useState<number>(
+    isOpen ? (router.query.id as unknown as number) : 1,
+  );
   const handleOpenChange = (state: boolean) => {
     if (!state) {
       router.push("/", undefined, { scroll: false });
@@ -17,19 +20,23 @@ export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   };
   return (
     <Dialog.Root open={isOpen} onOpenChange={handleOpenChange}>
-      <div className="flex flex-1 xl:max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-1 xl:max-w-[1280px] z-[1] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 w-full gap-8">
           {children}
           {localImages.map(({ id, ...rest }) => {
             const handleClick = () => {
+              setCurrentMovieId(id);
               router.push(`image/${id}`, undefined, { scroll: false });
             };
             return (
               <motion.div
                 key={id}
+                whileTap={{ scale: 0.98 }}
                 onClick={handleClick}
                 layoutId={`image-${id}`}
-                className={`relative flex mb-8 cursor-pointer`}
+                className={`relative flex mb-8 cursor-pointer ${
+                  currentMovieId === id ? "relative z-[1]" : ""
+                }`}
               >
                 <AspectBlurImage
                   alt="Unsplash Image"
